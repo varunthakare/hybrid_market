07b3bdcf0cb5e0a386ae741d9473bc13ba10d8cc
Varun Thakare <160563687+varunthakare@users.noreply.github.com>
2024-09-13 22:24:02 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,12 @@ import 'pages/intro_page.dart';
 void main() {
   runApp(const MyApp());
 }
-Varun bhai chi hava nusta dhurr
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  //shivam bhai murdabaad
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
